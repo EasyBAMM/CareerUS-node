424134b9aa78457b8b0afafbf5cc97a0e8c36cb5
EasyBAMM <huhjb1020@naver.com>
2021-09-21 21:24:28 +0900

    add write comment api

--- back-end/src/api/comments/comments.ctrl.js
+++ back-end/src/api/comments/comments.ctrl.js
@@ -39,6 +39,7 @@ export const write = async (ctx) => {
   const schema = Joi.object().keys({
     // 객체가 다음 필드를 가지고 있음을 검증
     text: Joi.string().required(),
+    parentComment: Joi.string(),
   });
   // 검증하고 나서 검증 실패인 경우 에러 처리
   const result = schema.validate(ctx.request.body);
@@ -47,35 +48,76 @@ export const write = async (ctx) => {
     ctx.body = result.error;
     return;
   }
+  // console.log('댓글생성 요청정보: ', ctx.request.body);
 
-  // 작성자_id 게시글_id, 댓글내용을 저장한다.
-  ctx.body = {
-    author: ctx.state.user._id,
-    post: ctx.state.post._id,
-    text: ctx.request.body.text,
-  };
+  // 부모댓글이 있다면 대댓글, 없다면 댓글
+  const { text, parentComment } = ctx.request.body;
+  if (parentComment) {
+    try {
+      // 1. 부모 댓글 정보가져오기
+      const parentCommentData = await Comment.findById(parentComment)
+        .lean()
+        .exec();
 
-  try {
-    // 댓글 생성
-    await Comment.create(ctx.body);
-    console.log('댓글 생성됨: ', ctx.body);
-  } catch (e) {
-    // 댓글 생성 실패
-    ctx.throw(500, e);
-    console.log('err: ', e);
-  }
+      // 2. 부모 댓글의 그룹번호를 물려받기, (깊이는 사실 필요없음 2단계까지만 표시)
+      const depth = parentCommentData.depth + 1;
+      const groupno = parentCommentData.groupno;
+      const inputComment = {
+        author: ctx.state.user._id,
+        post: ctx.state.post._id,
+        parentComment,
+        depth,
+        groupno,
+        text: text,
+      };
+      // 댓글 생성
+      const saveComment = await Comment.create(inputComment);
+      const comment = await Comment.find({ _id: saveComment._id })
+        .populate({ path: 'author', select: ['username', 'image'] })
+        .lean()
+        .exec();
+      ctx.body = comment;
+      // console.log('대댓글 생성됨: ', ctx.body);
+    } catch (e) {
+      // 대댓글 생성 실패
+      ctx.throw(500, e);
+      console.log('err: ', e);
+    }
+  } // 대댓글
+  else {
+    try {
+      // 댓글 생성 - 나머지 default 설정됨
+      const inputComment = {
+        author: ctx.state.user._id,
+        post: ctx.state.post._id,
+        text: text,
+      };
+      const saveComment = await Comment.create(inputComment);
+      const comment = await Comment.find({ _id: saveComment._id })
+        .populate({ path: 'author', select: ['username', 'image'] })
+        .lean()
+        .exec();
+
+      ctx.body = comment;
+      // console.log('댓글 생성됨: ', ctx.body);
+    } catch (e) {
+      // 댓글 생성 실패
+      ctx.throw(500, e);
+      console.log('err: ', e);
+    }
+  } // 댓글
 };
 
 /* 댓글 읽기
 GET /api/comments/:id
 */
 export const read = async (ctx) => {
-  const { id } = ctx.params;
+  const { id } = ctx.query;
+  console.log(ctx.query);
   if (!ObjectId.isValid(id)) {
     ctx.status = 400; // Bad Request
     return;
   }
-
   // 댓글 페이징 처리
   // query 는 문자열이기 때문에 숫자로 변환해주어야합니다.
   // 값이 주어지지 않았다면 1, 50 을 기본으로 사용합니다.
@@ -93,12 +135,12 @@ export const read = async (ctx) => {
     return;
   }
 
-  // 댓글 쿼리 처리
+  // 댓글 쿼리 처리 - 정렬중요(그룹번호로 먼저 출력, 같으면 입력날짜)
   const query = { post: id };
   // .populate({path: 'author', select: 'username'})
   try {
     const comments = await Comment.find(query)
-      .sort({ createdAt: orderBy })
+      .sort({ groupno: orderBy, createdAt: orderBy })
       .limit(limit)
       .skip(skip)
       .populate({ path: 'author', select: ['username', 'image'] })
