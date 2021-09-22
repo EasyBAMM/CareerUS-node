cd67daa393240344e4b0ed0221595a595a676eed
EasyBAMM <huhjb1020@naver.com>
2021-09-23 01:48:01 +0900

    add jwt user image

--- back-end/src/models/user.js
+++ back-end/src/models/user.js
@@ -38,6 +38,7 @@ UserSchema.methods.generateToken = function () {
       _id: this.id,
       username: this.username,
       name: this.name,
+      image: this.image,
     },
     process.env.JWT_SECRET, // 두 번째 파라미터에는 JWT 암호를 넣습니다.
     {
